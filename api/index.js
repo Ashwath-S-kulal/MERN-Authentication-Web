0c95c46afb2aa2f523acfbf810f87f8e82117268
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2025-07-30 13:36:27 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
-import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -12,12 +11,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err)=>{
     console.log(err)
 });
-const __dirname=path.resolve();
+
 const app = express();
-app.use(express.static(path.join(__dirname,'/client/dist')))
-app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,'client', 'dist' ,'index.html'))
-  });
+
+
+
 app.use(express.json());
 app.use(cookieParser());
 
