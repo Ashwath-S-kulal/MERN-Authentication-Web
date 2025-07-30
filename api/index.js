ad9731644ef57ba48c8da6068f43700d763cbf8b
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2025-07-30 13:51:26 +0530

    new

--- api/index.js
+++ api/index.js
@@ -7,11 +7,15 @@ import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('connected to MongoDB')
-}).catch((err)=>{
-    console.log(err)
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const __dirname = path.resolve();
 
 const app = express();
@@ -23,23 +27,22 @@ app.get('*', (req, res) => {
 });
 
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+  console.log('Server listening on port 3000');
 });
 
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
-app.use("/api/user",userRoutes);
-app.use("/api/auth",authRoutes);
-
-app.use((err,req,res,next)=>{
-  const statusCode=err.statusCode||500;
-  const message=err.message||'Internal Server Error';
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     message,
     statusCode,
   });
-
 });
